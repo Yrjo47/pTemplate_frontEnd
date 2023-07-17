f885cb0fa1dc0548c2ea7be105377962fd0e0fd0
Yrjo47 <arhegor@gmail.com>
2023-07-17 14:52:10 +0300

    added some components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import {Routes, Route} from 'react-router-dom'
+import Layout from './components/Layout';
 
 function App() {
   return (
-    <div className="App">
-
-    </div>
+    <Routes>
+      <Route path='/' element={<Layout />}>
+        
+      </Route>
+    </Routes>
   );
 }
 
